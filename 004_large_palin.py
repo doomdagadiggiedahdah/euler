698878e5f094452a7306fc67f60d192c503c838c
mat <matdallen@gmail.com>
2024-04-21 14:25:41 -0700

    004

--- 004_large_palin.py
+++ 004_large_palin.py
@@ -1,37 +1,30 @@
-# need to check if the product is a palindrome
-# start with the highest numbers and go down from there.
-# two 3-digit numbers.
-# check if palindrome
-# (strated late) (10:34:18) -- (10:36:59) 
-# also got wrong... (10:49:36) -- (11:18:04) stopped :,(
-# I think it's that I'm only adjusting one value at a time.
-# I'm confused at the None return. it was because I had changed the range to 10.
+## need to get the palindrome of the numbers, easy str(num) == str(num)[::-1]
+## now getting the highest value. I can see that the numbers need to be computed to see if palin
 
+# I'm uncertain how to go about finding the highest value of products in a set of numbers.
+## do I minus by one from each? or just by one side?
+## what if I did 99 * 99, 99 * 98, 99 * 97, ...and then 98 * 98, 98 * 97, 98 * 96...
+## computed all of them until -100 or something, and then returned the highest num?
 
-def nice():
-    a = 999
-    b = 999
-    count = 0
+## ok check it: 99X * (matches, then descending) and if palin, append to list and then
+## go to 99(X-1) * (matches, descending), etc.
+## I don't really like this, but it should provide the answer.
 
-    while True:
-        num = a * b 
-        if str(num) == str(num)[::-1]:
-            print(a,b)
-            return num 
+## (14:15:50) -- (14:24:17) 
+## I actually did take longer than this, maybe another 10?
 
-        if count % 2 == 1:
-            a -= 1
-        else:
-            b -= 1
+pals = []
 
-        count += 1
-print(nice())
+up_lim = 999
+dw_lim = 100
 
+## oh, the b loop needs to be diff; otherwise it's just computing the same thing each time.
+## 
 
-
-# so now what am I thinking? how should I do this?
-# what the hell.
-# shouldn't get frustrated. take it step by step.
-# wnat to find the largest number, 
-# I'm definitely getting the palindrome right.
-# gonna come back to this.
+for a in range(up_lim, dw_lim, -1):
+    for b in range(a, dw_lim, -1):
+        if str(a*b) == str(a*b)[::-1]:
+            pals.append(a*b)
+            break
+pals.sort()
+print(pals[-1])
