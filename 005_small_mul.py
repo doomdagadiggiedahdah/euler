16dce07d211c877096860abac793ef5dc175bb9d
mat <matdallen@gmail.com>
2024-05-18 18:52:01 -0700

    013 and 014

--- 005_small_mul.py
+++ 005_small_mul.py
@@ -1,28 +1,34 @@
-# find smallest number
-# can be divided by 1-20 with no remainder
-# note: I already have a test case to check if it's right.
-# (11:21:23) -- stopping for now (11:44:30) 
-# it needs to be divisible by ALL numbers. try
-# start from 0 and see if divisible. Maybe I'm getting ahead of myself.
+## (10:01:57) 
+## if it makes past all these numbers, then it's the winner, right? just return that.
+## how do I tell if goes through all of these though? 
 
-def sml(rng):
-    count = 252
-    for i in range(2432902008176640000):
-        for i in range(1, rng + 1):
-            if count % i != 0:
-                count += 1
-                print(count)
+optimized_nums = [2,3,5,7,11,13,17,19] # huh, the primes?
+optimized_nums = [2,3,5,7,10] # huh, the primes?
+
+def get_it():
+    #answer = 2520
+    answer = 1
+    while True:
+        for i in optimized_nums:
+            if answer % i != 0:
+                answer += 1
                 continue
-print(sml(10))
+            else:
+                return answer
+
+print(get_it())
 
 
 
-# need a number to check. count.
-# needs to be divisible by the numbers through range
-# how do I check if a number is k
-# now I'm getting the correct result, but not passing it back correctly.
-# I could just hack it with this to get the right answer.
-# maybe a less computationally expensive way would be to take 20! and then go down from there.
-# damn, 20! == 2432902008176640000. shows even a shallow need for thinking correctly before running.
-# I also don't know how to stop this thing. with the test I don't know how to say "all values
-# are divisible, 
+## a naive and expensive way would be to: i % range(1,21) == 0 then append results
+## but we should be able to remove computations to get rid of numbers to run
+## like if a number is divisible by 10, it's also divisible by 2 and 5, so you can remove dividing by 10. That would be a thing to solve at first.
+## let's get the example to work first
+
+## num = 2520 # because we know that the limit of last one
+## for i in range(1,21):
+    ## if num % i != 0:
+        ## continue
+## but if it makes it past....
+
+## so how do I test if it goes through all of the numbers? right now it's getting stoppd at a single number. I should be able to do a list comp, right?
